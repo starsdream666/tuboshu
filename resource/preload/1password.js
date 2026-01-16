3a8429ce51cc693e89891bf741e097bbdb0796cb
starsdream666 <boyuliu894@gmail.com>
2026-01-16 14:11:57 +0800

    docs: add clear instructions for getting unpacked 1Password extension

--- resource/preload/1password.js
+++ resource/preload/1password.js
@@ -1,8 +1,9 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer, shell } = require('electron');
 
 contextBridge.exposeInMainWorld('onePasswordApi', {
     getConfig: () => ipcRenderer.invoke('1password:get-config'),
     setConfig: (config) => ipcRenderer.invoke('1password:set-config', config),
     validatePath: (path) => ipcRenderer.invoke('1password:validate-path', path),
     selectFolder: () => ipcRenderer.invoke('1password:select-folder'),
+    openExternal: (url) => shell.openExternal(url),
 });
